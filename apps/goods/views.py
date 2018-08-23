9450d8d32de7a5e487ca3c41e2f8d3361e8eeafb
bwisgood <857464370@qq.com>
2018-08-23 16:28:01 +0800

    jwt auth

---
+++ apps/goods/views.py
@@ -0,0 +1,86 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+# 三级
+from rest_framework import mixins
+from rest_framework import generics
+# 分页定制化
+from rest_framework.pagination import PageNumberPagination
+# 五阶 Viewset
+from rest_framework import viewsets
+# 使用django-filter过滤
+from django_filters.rest_framework import DjangoFilterBackend
+# 使用rest_framework的filter做搜索
+from rest_framework import filters
+# 配置单独的认证
+from rest_framework.authentication import TokenAuthentication
+
+from .serializers import GoodsSerializer, CategorySerializer
+from .models import Goods, GoodsCategory
+from .filters import GoodsFilter
+
+
+class GoodsPagination(PageNumberPagination):
+    page_size = 12
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    page_query_param = 'page'
+
+
+class GoodsListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Goods.objects.all()
+    # queryset = Goods.objects.all().filter(category__parent_category__parent_category_id=1)
+    serializer_class = GoodsSerializer
+    pagination_class = GoodsPagination
+    # 使用django-filter过滤配置backends
+    # 使用rest_framework的filter做搜索SearchFilter
+    # 使用rest_framework的filter做排序OrderingFilter
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    # 增加ordering_fields 需要排序的字段
+    ordering_fields = ('sold_num', 'shop_price')
+    # filter_fields
+    # filter_fields = ('name','shop_price')
+    # 自定义django-filter 将写好的filters.py中的filter配置到filter_class
+    filter_class = GoodsFilter
+    # 使用rest_framework的filter做搜索
+    search_fields = ("name", 'goods_brief', 'goods_desc')
+    # 将需要认证的view单独在view中声明
+    # 公开数据
+    # authentication_classes = (TokenAuthentication,)
+
+    # filter过滤返回结果 重写默认的get_queryset方法
+    # def get_queryset(self):
+    #     queryset = Goods.objects.all()
+    #     # 获取get请求参数
+    #     price_min = self.request.query_params.get("price_min", 0)
+    #     if price_min:
+    #         queryset = Goods.objects.filter(shop_price__gt=int(price_min))
+    #     return queryset
+
+    def post(self, request, format=None):
+        """
+        获取数据 创建good
+        :param request:
+        :param format:
+        :return:
+        """
+        # request.data 可以方便获取到接收的参数 无论是GET POST BODY
+        serializer = GoodsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class RetrieveMixView(viewsets.ViewSetMixin, generics.RetrieveAPIView):
+#     pass
+
+
+class CategoryListViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    """
+    list:
+        商品分类列表数据
+    """
+    queryset = GoodsCategory.objects.all()
+    # queryset = GoodsCategory.objects.all()
+    serializer_class = CategorySerializer
---
+++ imooc_drf/urls.py
@@ -0,0 +1,66 @@
+"""imooc_drf URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+# from django.contrib import admin
+import xadmin
+from imooc_drf.settings import MEDIA_ROOT
+from django.views.static import serve
+# drf
+from rest_framework.documentation import include_docs_urls
+
+from goods.views import GoodsListViewSet, CategoryListViewSet
+
+# 五阶 viewSet 配置路由
+# goods_list = GoodsListViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+
+# 六阶 配置router
+from rest_framework.routers import DefaultRouter
+# 配置获取token 的api
+from rest_framework.authtoken import views
+
+# 六阶 配置router
+router = DefaultRouter()
+router.register(r'goods', GoodsListViewSet, base_name="goods_list")
+router.register(r'categorys', CategoryListViewSet, base_name="categorys")
+# jwt导入url
+from rest_framework_jwt.views import obtain_jwt_token
+
+print(router.urls)
+# 配置单条信息获取
+category_one = CategoryListViewSet.as_view({
+    'get': 'retrieve'
+})
+
+urlpatterns = [
+    url(r'^xadmin/', xadmin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # 商品列表页
+    # 六阶 注释原有的goods获取路由
+    # url(r'goods/$', goods_list, name="goods_list"),
+    # 六阶 新增所有router配置
+    url(r"^", include(router.urls)),
+    url(r"^categorys/(?P<pk>[0-9]+)/$", category_one, name="single_info"),
+    url(r"^docs/", include_docs_urls(title="bw")),
+    # 配置获取token 的api地址
+    # drf自带的token认证模式
+    url(r'^api-token-auth/', views.obtain_auth_token),
+    # jwt的认证接口
+    url(r"^login/", obtain_jwt_token)
+]
